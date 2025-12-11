8d33cb363968a9f8979291832891dd9e0213d487
ErdeiGergo <21.erdei.gergo.janos@mechwart.com>
2025-12-11 11:31:10 +0100

    Kattintásra fordul a kártya (nem hover)

--- src/components/CardTable.tsx
+++ src/components/CardTable.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import type { CardType } from '../App'
 
 const CardTable = ({card, nextCard}: {card: CardType, nextCard: () => void}) => {
 
+    const [turned, setTurned] = useState<Boolean>(false)
+
+    function turn() {
+        setTurned(!turned)
+    }
+
     return (
-    <div className='flip-card'>
+    <div className={`flip-card ${turned ? "turned":""}`} onClick={turn}>
         <div className='flip-card-inner'>
             <div className={`flip-card-front ${card.points >= 0 ? "green":"red"}`}>
                 <h1>{card.question}</h1>
@@ -13,13 +19,13 @@ const CardTable = ({card, nextCard}: {card: CardType, nextCard: () => void}) => 
                 <h1 className={`points ${card.points >= 0 ? "green":"red"}`}>{card.points}</h1>
                 <h2>{card.answer}</h2>
                 <div className='buttons'>
-                    <div className='eltalaltam'>
+                    <div className='eltalaltam' onClick={nextCard}>
                         <p>Eltaláltam</p>
-                        <i onClick={nextCard} className="fa-regular fa-circle-check"></i>
+                        <i className="fa-regular fa-circle-check"></i>
                     </div>
-                    <div className='nemsikerult'>
+                    <div className='nemsikerult' onClick={nextCard}>
                         <p>Nem sikerült</p>
-                        <i onClick={nextCard} className="fa-regular fa-circle-xmark"></i>
+                        <i className="fa-regular fa-circle-xmark"></i>
                     </div>
                 </div>
             </div>
